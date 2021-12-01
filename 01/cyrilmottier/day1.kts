a1ec14aa9938ed9ce193f78fc9b8e8362ea6360b
Cyril Mottier <cyril@gensdeconfiance.com>
2021-12-01 10:10:17 +0100

    Use my own input^

--- 01/cyrilmottier/day1.kts
+++ 01/cyrilmottier/day1.kts
@@ -1,7 +1,6 @@
 import java.io.File
 
 val largerCount = File("input.txt").readLines()
-    .filter { it.length > 0 }
     .zipWithNext()
     .map { it.second.toInt() - it.first.toInt() }
     .count { it > 0 }
