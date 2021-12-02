032f682ce640ea2368adf304333a3461c7a5a88a
hugo <hugo@gensdeconfiance.com>
2021-12-02 09:37:38 +0100

    rivten - day 02 - part 2 - haskell

--- 02/rivten/Main.hs
+++ 02/rivten/Main.hs
@@ -8,7 +8,14 @@ acc (x, y) ("down", a) = (x, y + a)
 acc (x, y) ("up", a) = (x, y - a)
 acc _ _ = undefined
 
+acc2 :: (Int, Int, Int) -> ([Char], Int) -> (Int, Int, Int)
+acc2 (x, y, aim) ("forward", a) = (x + a, y + aim * a, aim)
+acc2 (x, y, aim) ("down", a) = (x, y, aim + a)
+acc2 (x, y, aim) ("up", a) = (x, y, aim - a)
+acc2 _ _ = undefined
+
 main :: IO ()
 main = do
     content <- hGetContents stdin
     print $ (\(x, y) -> x * y) $ foldl acc (0,0) $ map ((\[a,b] -> (a, read b)) . words) $ lines content
+    print $ (\(x, y, _) -> x * y) $ foldl acc2 (0,0, 0) $ map ((\[a,b] -> (a, read b)) . words) $ lines content
