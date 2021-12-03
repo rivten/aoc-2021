42aca2db12fe992d101ef66130b6b2f7d7521b67
nicolas-lair <25099303+nicolas-lair@users.noreply.github.com>
2021-12-03 21:05:31 +0100

    Update day3.py

--- 03/nicolr/day3.py
+++ 03/nicolr/day3.py
@@ -2,13 +2,13 @@ from collections import Counter
 
 if __name__ == "__main__":
     with open('input.txt') as f:
-        lines = f.readlines()
+        lines = f.read().splitlines()
 
     first_line = lines.pop()
     counter_dict = {i: Counter([d]) for i, d in enumerate(first_line)}
     for line in lines:
-        [counter_dict[i].update([d]) for i,d in enumerate(first_line)]
-    
+        [counter_dict[i].update([d]) for i,d in enumerate(line)]
+
     most_frequent = [counter_.most_common()[0][0] for counter_ in counter_dict.values()]
     least_frequent = [str(1 - int(d)) for d in most_frequent]
     
