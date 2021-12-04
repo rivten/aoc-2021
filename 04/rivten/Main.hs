579f821940091ff7b0cb5f79b2a24ff6956dafea
hugo <hugo@gensdeconfiance.com>
2021-12-04 11:47:03 +0100

    rivten - day 04 - part 2 - haskell

--- 04/rivten/Main.hs
+++ 04/rivten/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Applicative
 import Data.List.Split
 import Data.List
 
@@ -10,6 +11,10 @@ getInputData s = let firstLine:remaining = splitOn "\n\n" s
 isVictoryTable :: [[Int]] -> Bool
 isVictoryTable t = (any null t) || (any null $ transpose t)
 
+-- NOTE: this logic is just plain wrong...
+-- It just happened to work for my sample input.
+-- You cannot remove element from a table otherwise it causes
+-- mismatch in the transpose
 removeElemFromTable :: Int -> [[Int]] -> [[Int]]
 removeElemFromTable e = map (filter (/= e))
 
@@ -22,5 +27,26 @@ partOne = aux undefined
                in if not $ null victoryTables then (sum $ map sum $ head victoryTables) * previous
                                               else aux n ns (map (removeElemFromTable n) bingoTables)
 
+isRealVictoryTable :: [[Int]] -> [Int] -> Bool
+isRealVictoryTable table ns = (any (all (`elem` ns)) table) || (any (all (`elem` ns)) $ transpose table)
+
+isInTable :: [[Int]] -> Int -> Bool
+isInTable t e = any (elem e) t
+
+getRealTableScore :: [[Int]] -> [Int] -> Int
+getRealTableScore table es = ((sum $ map sum table) - (sum $ filter (isInTable table) es)) * (head es)
+
+partTwo :: [Int] -> [[[Int]]] -> Int
+partTwo numbers tables = aux numbers tables 0 []
+    where aux [] _ lastVictoryScore _ = lastVictoryScore
+          aux (n:ns) notYetWonTables lastVictoryScore listOfProcessedNumbers = 
+              let newListOfProcessedNumbers = n:listOfProcessedNumbers
+                  newVictoryTables = filter (\t -> isRealVictoryTable t newListOfProcessedNumbers) notYetWonTables
+                  newNotYetWonTables = filter (\t -> not $ isRealVictoryTable t newListOfProcessedNumbers) notYetWonTables
+               in if null newVictoryTables then aux ns newNotYetWonTables lastVictoryScore newListOfProcessedNumbers
+                                           else let newLastVistoryScore = getRealTableScore (head newVictoryTables) newListOfProcessedNumbers
+                                                 in aux ns newNotYetWonTables newLastVistoryScore newListOfProcessedNumbers
+
+
 main :: IO ()
-main = interact $ show . (uncurry partOne) . getInputData
+main = interact $ show . liftA2 (,) (uncurry partOne) (uncurry partTwo) . getInputData
