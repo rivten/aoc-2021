d953a6ef5261fcab5f98a4be3aaf328b448098e4
Cyril Mottier <cyril@gensdeconfiance.com>
2021-12-05 14:31:50 +0100

    Review day03 and add day03-part2

--- 03/cyrilmottier/day03.kts
+++ 03/cyrilmottier/day03.kts
@@ -1,10 +1,6 @@
 import java.io.File
 import kotlin.math.pow
 
-// Can't find something to do this in Kotlin stdlib…
-// I'm probably missing something
-fun Int.bitIndexToDecimal() = 2.0.pow(this).toInt()
-
 val lines = File("input.txt").readLines()
 val wordSize = lines[0].length
 
@@ -18,14 +14,12 @@ val delta = lines.fold(List(wordSize) { 0L }) { wordAcc, word ->
 // what happens if there's as much 1's as 0's. But gamma rate is the
 // inverse of epsilon so it doesn't matter.
 val gamma = delta
-    .map { if (it > 0) 1 else 0 }
-    .reversed()
-    .foldIndexed(0) { index, acc, bit ->
-        acc + bit * index.bitIndexToDecimal()
-    }
+    .map { if (it > 0) "1" else "0" }
+    .joinToString("")
+    .toInt(2)
 
 // Let's have fun with bits!
-val epsilon = gamma.inv() and (wordSize.bitIndexToDecimal() - 1)
+val epsilon = gamma.inv() and (2.0.pow(wordSize).toInt() - 1)
 
 println("gamma: ${Integer.toBinaryString(gamma)} ($gamma)")
 println("epsilon: ${Integer.toBinaryString(epsilon)} ($epsilon)")
