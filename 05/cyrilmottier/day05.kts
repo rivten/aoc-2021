490e72be8e3d983cf43ec3f8b4e0bd8639ebddf3
Cyril Mottier <cyril@gensdeconfiance.com>
2021-12-05 14:11:02 +0100

    Add day 05

---
+++ 05/cyrilmottier/day05.kts
@@ -0,0 +1,35 @@
+import java.io.File
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.sign
+
+val result = File("input.txt").readLines()
+    .map { line ->
+        line.split(" -> ").map { points ->
+            points.split(",").map { it.toInt() }
+        }
+    }
+    .mapNotNull { (start, end) ->
+        val (x1, y1) = start
+        val (x2, y2) = end
+
+        if (x1 == x2 || y1 == y2 ) {
+            val delta = max(abs(x2 - x1), abs(y2 - y1))
+            val signX = (x2 - x1).sign
+            val signY = (y2 - y1).sign
+            (0..delta).map { (x1 + it * signX) to (y1 + it * signY) }
+        } else {
+            null
+        }
+    }
+    .fold(mapOf<Pair<Int, Int>, Int>()) { space, line ->
+        buildMap {
+            putAll(space)
+            line.forEach { point ->
+                put(point, space.getOrDefault(point, 0) + 1)
+            }
+        }
+    }
+    .count { it.value > 1 }
+
+println(result)
