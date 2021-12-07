70a23ca22c316d04c38a4975dc357ab5a3ab0239
hugo <hugo@gensdeconfiance.com>
2021-12-07 14:24:47 +0100

    rivten - day 07 - part 2 - haskell

--- 07/rivten/Main.hs
+++ 07/rivten/Main.hs
@@ -11,5 +11,12 @@ sumOfFuelToGetTo positions destination = sum $ map (\p -> abs (p - destination))
 solve1 :: [Int] -> Int
 solve1 l = minimum $ map (sumOfFuelToGetTo l) [(minimum l)..(maximum l)]
 
+sumOfFuelToGetTo2 :: [Int] -> Int -> Int
+sumOfFuelToGetTo2 positions destination = sum $ map (\p -> let n = abs (p - destination) in div (n * (n + 1)) 2) positions
+
+solve2 :: [Int] -> Int
+solve2 l = minimum $ map (sumOfFuelToGetTo2 l) [(minimum l)..(maximum l)]
+
 main :: IO ()
-main = interact $ show . solve1 . parseInput
+--main = interact $ show . solve1 . parseInput
+main = interact $ show . solve2 . parseInput
