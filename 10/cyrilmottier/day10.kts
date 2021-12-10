b62968929306605ac9cc7f63129f29b42e6888a9
Cyril Mottier <cyril@gensdeconfiance.com>
2021-12-10 10:43:46 +0100

    Add part2
    I had a Kotlin compiler error… don’t know why so I had to do a bit of trick

--- 10/cyrilmottier/day10.kts
+++ 10/cyrilmottier/day10.kts
@@ -18,35 +18,74 @@ val SCORE = mapOf(
     '>' to 25137,
 )
 
+val SCORE_PART2 = mapOf(
+    ')' to 1,
+    ']' to 2,
+    '}' to 3,
+    '>' to 4,
+)
 
 sealed class Validity
 object Valid : Validity()
-class Corrupted(val expected: Char, val found: Char) : Validity()
-object Incomplete : Validity()
+class Corrupted(
+    val expected: Char,
+    val found: Char
+    ) : Validity()
+class Incomplete(
+    private val stack: List<Char>
+) : Validity() {
+
+    val missingChars = stack.reversed()
+        .map { // Should be "it -> REVERSE_PAIRS[it]!!" but I have a Kotlin compiler error :s
+            when(it) {
+                '<' -> '>'
+                '[' -> ']'
+                '(' -> ')'
+                '{' -> '}'
+                else -> throw IllegalStateException()
+            }
+        }
+
+}
 
 fun computeValidity(input: String): Validity {
-    val stack = Stack<Char>()
+    val stack = mutableListOf<Char>()
     input.forEach {
         when {
-            it in PAIRS.values -> stack.push(it)
+            it in PAIRS.values -> stack.add(it)
             it in PAIRS.keys -> {
                 if (stack.isEmpty()) {
                     throw IllegalStateException() // Not defined in the exercise
                 }
-                if (stack.peek() != PAIRS[it]) {
-                    return Corrupted(REVERSE_PAIRS[stack.peek()]!!, it)
+                if (stack[stack.size - 1] != PAIRS[it]) {
+                    return Corrupted(REVERSE_PAIRS[stack[stack.size - 1]]!!, it)
                 }
-                stack.pop()
+                stack.removeLast()
             }
         }
     }
-    return if (!stack.isEmpty()) Incomplete else Valid
+    return if (!stack.isEmpty()) Incomplete(stack) else Valid
 }
 
-val resultPart1 = File("input.txt").readLines()
+val validities = File("input.txt").readLines()
     .map { computeValidity(it) }
+
+val resultPart1 = validities
     .mapNotNull { if (it is Corrupted) it else null }
     .map { SCORE[it.found]!! }
     .sum()
 
-println("part1: resultPart1")
+val resultPart2 = validities
+    .mapNotNull { if (it is Incomplete) it else null }
+    .map { it.missingChars }
+    .map { line ->
+        line.fold(0L) { acc, item -> acc * 5 + SCORE_PART2[item]!! }
+    }
+    .sorted()
+    .let {
+        it[it.size / 2]
+    }
+
+
+println("part1: $resultPart1")
+println("part2: $resultPart2")
