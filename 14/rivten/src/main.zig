b50d86aa80543cacf10a74949d0dcfd27b388606
hugo <hugo@gensdeconfiance.com>
2021-12-14 18:38:41 +0100

    rivten - day 14 - part 2 - zig

--- 14/rivten/src/main.zig
+++ 14/rivten/src/main.zig
@@ -38,22 +38,40 @@ fn buildRuleListFromInput(input_data: []const u8) !ParsedInput {
     };
 }
 
-fn doOneSim(template: []const u8, rules: []const Rule) !std.ArrayList(u8) {
-    var result = std.ArrayList(u8).init(std.heap.page_allocator);
+fn doOneSim(template: Template, rules: []const Rule) !Template {
+    var result = Template{
+        .bi = try template.bi.clone(),
+        .elemCount = try template.elemCount.clone(),
+    };
 
-    for (template) |c, index| {
-        try result.append(c);
-        if (index + 1 < template.len) {
-            const nextChar = template[index + 1];
-            var foundRule: ?Rule = null;
-            for (rules) |rule| {
-                if (rule.firstChar == c and rule.secondChar == nextChar) {
-                    foundRule = rule;
-                    break;
-                }
+    for (rules) |rule| {
+        var bi = [2]u8{ rule.firstChar, rule.secondChar };
+        if (template.bi.get(bi)) |count| {
+            var biA = [2]u8{ rule.firstChar, rule.result };
+            var biB = [2]u8{ rule.result, rule.secondChar };
+            var getOrPutResultA = try result.bi.getOrPut(biA);
+            if (getOrPutResultA.found_existing) {
+                getOrPutResultA.value_ptr.* += count;
+            } else {
+                getOrPutResultA.value_ptr.* = count;
             }
-            if (foundRule) |rule| {
-                try result.append(rule.result);
+            var getOrPutResultB = try result.bi.getOrPut(biB);
+            if (getOrPutResultB.found_existing) {
+                getOrPutResultB.value_ptr.* += count;
+            } else {
+                getOrPutResultB.value_ptr.* = count;
+            }
+
+            var getOrPutResultBi = try result.bi.getOrPut(bi);
+            if (getOrPutResultBi.found_existing) {
+                getOrPutResultBi.value_ptr.* -= count;
+            }
+
+            var gopr = try result.elemCount.getOrPut(rule.result);
+            if (gopr.found_existing) {
+                gopr.value_ptr.* += count;
+            } else {
+                gopr.value_ptr.* = count;
             }
         }
     }
@@ -61,19 +79,14 @@ fn doOneSim(template: []const u8, rules: []const Rule) !std.ArrayList(u8) {
     return result;
 }
 
-fn computeTemplateValue(template: []const u8) !usize {
-    var h = std.AutoHashMap(u8, usize).init(std.heap.page_allocator);
-    defer h.deinit();
-    for (template) |c| {
-        if (h.get(c)) |countC| {
-            try h.put(c, countC + 1);
-        } else {
-            try h.put(c, 1);
-        }
-    }
+const Template = struct {
+    bi: std.AutoHashMap([2]u8, usize),
+    elemCount: std.AutoHashMap(u8, usize),
+};
+
+fn computeTemplateValue(h: std.AutoHashMap(u8, usize)) !usize {
     var maxValue: usize = std.math.minInt(usize);
     var minValue: usize = std.math.maxInt(usize);
-
     var it = h.valueIterator();
     while (it.next()) |v| {
         if (v.* > maxValue) {
@@ -86,22 +99,45 @@ fn computeTemplateValue(template: []const u8) !usize {
     return maxValue - minValue;
 }
 
+fn buildTemplateFromInput(i: []const u8) !Template {
+    var oldTemplate = Template{
+        .bi = std.AutoHashMap([2]u8, usize).init(std.heap.page_allocator),
+        .elemCount = std.AutoHashMap(u8, usize).init(std.heap.page_allocator),
+    };
+    for (i) |c, cIndex| {
+        if (cIndex + 1 < i.len) {
+            var bi = [2]u8{ c, i[cIndex + 1] };
+            var getOrPutResult = try oldTemplate.bi.getOrPut(bi);
+            if (getOrPutResult.found_existing) {
+                getOrPutResult.value_ptr.* += 1;
+            } else {
+                getOrPutResult.value_ptr.* = 1;
+            }
+        }
+
+        var gopr = try oldTemplate.elemCount.getOrPut(c);
+        if (gopr.found_existing) {
+            gopr.value_ptr.* += 1;
+        } else {
+            gopr.value_ptr.* = 1;
+        }
+    }
+    return oldTemplate;
+}
+
 pub fn main() anyerror!void {
     const parsedInput = try buildRuleListFromInput(input);
     for (parsedInput.rules.items) |rule| {
         std.log.info("{} {} -> {}", .{ rule.firstChar, rule.secondChar, rule.result });
     }
 
-    var oldTemplate = std.ArrayList(u8).init(std.heap.page_allocator);
-    for (parsedInput.chain) |c| {
-        try oldTemplate.append(c);
-    }
+    var oldTemplate = try buildTemplateFromInput(parsedInput.chain);
     var iterIndex: usize = 0;
-    while (iterIndex < 10) : (iterIndex += 1) {
-        var newTemplate = try doOneSim(oldTemplate.items, parsedInput.rules.items);
-        oldTemplate.deinit();
-        std.log.info("After step {}: {s} {}", .{ iterIndex + 1, newTemplate.items, newTemplate.items.len });
-        std.log.info("{}", .{computeTemplateValue(newTemplate.items)});
+    while (iterIndex < 40) : (iterIndex += 1) {
+        var newTemplate = try doOneSim(oldTemplate, parsedInput.rules.items);
+        oldTemplate.bi.deinit();
+        oldTemplate.elemCount.deinit();
+        std.log.info("{} {}", .{ iterIndex + 1, computeTemplateValue(newTemplate.elemCount) });
         oldTemplate = newTemplate;
     }
 }
