970fa6409d902592f5fe844fb1c3fe71a01887c3
hugo <hugo@gensdeconfiance.com>
2021-12-27 20:07:51 +0100

    trying out day 16 part 2 without success

--- 16/rivten/src/main.rs
+++ 16/rivten/src/main.rs
@@ -14,43 +14,47 @@ fn char_to_hex(c: &u8) -> u8 {
 
 #[derive(Debug)]
 enum PacketData {
-    Operator(OperatorPacket),
     LiteralValue(LiteralValuePacket),
+    Sum(OperatorPacket),
+    Product(OperatorPacket),
+    Min(OperatorPacket),
+    Max(OperatorPacket),
+    GreaterThan(OperatorPacket),
+    LessThan(OperatorPacket),
+    EqualTo(OperatorPacket),
 }
 
-#[derive(Debug)]
-struct PacketHeader {
-    version: u8,
-}
+//#[derive(Debug)]
+//struct PacketHeader {
+//    version: u8,
+//}
 
 #[derive(Debug)]
 struct OperatorPacket {
-    header: PacketHeader,
-    //length_type_id: u8,
+    //header: PacketHeader,
     subpackets: Vec<Box<PacketData>>,
 }
 
 #[derive(Debug)]
 struct LiteralValuePacket {
-    header: PacketHeader,
+    //header: PacketHeader,
     value: u32,
 }
 
 type BR<'a> = bitstream_io::read::BitReader<&'a [u8], bitstream_io::BigEndian>;
 
-fn parse_packet_header(bit_reader: &mut BR) -> (u32, PacketHeader) {
+fn parse_packet_header(bit_reader: &mut BR) -> (u32, ()) {
+    let _x = bit_reader.read::<u8>(3);
     (
         3,
-        PacketHeader {
-            version: bit_reader.read::<u8>(3).unwrap(),
-        },
+        ()
+        //PacketHeader {
+        //    version: bit_reader.read::<u8>(3).unwrap(),
+        //},
     )
 }
 
-fn parse_literal_value_packet(
-    bit_reader: &mut BR,
-    header: PacketHeader,
-) -> (u32, LiteralValuePacket) {
+fn parse_literal_value_packet(bit_reader: &mut BR) -> (u32, LiteralValuePacket) {
     let mut value = 0;
     let mut bits_parsed = 0;
     while bit_reader.read::<u8>(1).unwrap() == 1 {
@@ -64,10 +68,11 @@ fn parse_literal_value_packet(
     value += bit_reader.read::<u32>(4).unwrap();
     bits_parsed += 5;
 
-    (bits_parsed, LiteralValuePacket { header, value })
+    //(bits_parsed, LiteralValuePacket { header, value })
+    (bits_parsed, LiteralValuePacket { value })
 }
 
-fn parse_operator_packet(bit_reader: &mut BR, header: PacketHeader) -> (u32, OperatorPacket) {
+fn parse_operator_packet(bit_reader: &mut BR) -> (u32, OperatorPacket) {
     let mut bits_parsed = 0;
 
     let length_type_id = bit_reader.read::<u8>(1).unwrap();
@@ -104,7 +109,7 @@ fn parse_operator_packet(bit_reader: &mut BR, header: PacketHeader) -> (u32, Ope
     (
         bits_parsed,
         OperatorPacket {
-            header,
+            //header,
             //length_type_id,
             subpackets,
         },
@@ -112,45 +117,116 @@ fn parse_operator_packet(bit_reader: &mut BR, header: PacketHeader) -> (u32, Ope
 }
 
 fn parse_packet_data(bit_reader: &mut BR) -> (u32, PacketData) {
-    let (bits_parsed_for_header, header) = parse_packet_header(bit_reader);
+    let (bits_parsed_for_header, _) = parse_packet_header(bit_reader);
     let mut bits_parsed = bits_parsed_for_header;
     //println!("{:?}", header);
     let packet_type = bit_reader.read::<u8>(3).unwrap();
     bits_parsed += 3;
     match packet_type {
+        0 => {
+            let (bits_parsed_for_packet, op) = parse_operator_packet(bit_reader);
+            (bits_parsed + bits_parsed_for_packet, PacketData::Sum(op))
+        }
+        1 => {
+            let (bits_parsed_for_packet, op) = parse_operator_packet(bit_reader);
+            (
+                bits_parsed + bits_parsed_for_packet,
+                PacketData::Product(op),
+            )
+        }
+        2 => {
+            let (bits_parsed_for_packet, op) = parse_operator_packet(bit_reader);
+            (bits_parsed + bits_parsed_for_packet, PacketData::Min(op))
+        }
+        3 => {
+            let (bits_parsed_for_packet, op) = parse_operator_packet(bit_reader);
+            (bits_parsed + bits_parsed_for_packet, PacketData::Max(op))
+        }
         4 => {
-            let (bits_parsed_for_packet, lit_value) =
-                parse_literal_value_packet(bit_reader, header);
+            let (bits_parsed_for_packet, lit_value) = parse_literal_value_packet(bit_reader);
             (
                 bits_parsed + bits_parsed_for_packet,
                 PacketData::LiteralValue(lit_value),
             )
         }
-        _ => {
-            let (bits_parsed_for_packet, op) = parse_operator_packet(bit_reader, header);
+        5 => {
+            let (bits_parsed_for_packet, op) = parse_operator_packet(bit_reader);
             (
                 bits_parsed + bits_parsed_for_packet,
-                PacketData::Operator(op),
+                PacketData::GreaterThan(op),
             )
         }
+        6 => {
+            let (bits_parsed_for_packet, op) = parse_operator_packet(bit_reader);
+            (
+                bits_parsed + bits_parsed_for_packet,
+                PacketData::LessThan(op),
+            )
+        }
+        7 => {
+            let (bits_parsed_for_packet, op) = parse_operator_packet(bit_reader);
+            (
+                bits_parsed + bits_parsed_for_packet,
+                PacketData::EqualTo(op),
+            )
+        }
+        _ => panic!(),
     }
 }
 
-fn sum_of_version(packet: PacketData) -> u32 {
+//fn sum_of_version(packet: PacketData) -> u32 {
+//    match packet {
+//        PacketData::Operator(op) => {
+//            let mut sum = op.header.version as u32;
+//            for p in op.subpackets {
+//                sum += sum_of_version(*p);
+//            }
+//            sum
+//        }
+//        PacketData::LiteralValue(lit) => lit.header.version as u32,
+//    }
+//}
+
+fn evaluate(packet: &PacketData) -> u128 {
     match packet {
-        PacketData::Operator(op) => {
-            let mut sum = op.header.version as u32;
-            for p in op.subpackets {
-                sum += sum_of_version(*p);
+        PacketData::LiteralValue(lit) => lit.value as u128,
+        PacketData::Sum(op) => op.subpackets.iter().map(|p| evaluate(&p)).sum(),
+        PacketData::Product(op) => op.subpackets.iter().map(|p| evaluate(&p)).product(),
+        PacketData::Min(op) => op.subpackets.iter().map(|p| evaluate(&p)).min().unwrap(),
+        PacketData::Max(op) => op.subpackets.iter().map(|p| evaluate(&p)).max().unwrap(),
+        PacketData::GreaterThan(op) => {
+            let ev_a = evaluate(&op.subpackets[0]);
+            let ev_b = evaluate(&op.subpackets[1]);
+            if ev_a > ev_b {
+                1
+            } else {
+                0
             }
-            sum
         }
-        PacketData::LiteralValue(lit) => lit.header.version as u32,
+        PacketData::LessThan(op) => {
+            let ev_a = evaluate(&op.subpackets[0]);
+            let ev_b = evaluate(&op.subpackets[1]);
+            if ev_a < ev_b {
+                1
+            } else {
+                0
+            }
+        }
+        PacketData::EqualTo(op) => {
+            let ev_a = evaluate(&op.subpackets[0]);
+            let ev_b = evaluate(&op.subpackets[1]);
+            if ev_a == ev_b {
+                1
+            } else {
+                0
+            }
+        }
     }
 }
 
 fn main() {
     let file_content = std::fs::read_to_string("input.txt").unwrap();
+    //let file_content = std::fs::read_to_string("sample.txt").unwrap();
     let file_content = file_content.trim();
 
     let bytes = file_content
@@ -173,5 +249,5 @@ fn main() {
 
     let (_, packet) = parse_packet_data(&mut bit_reader);
     println!("{:?}", packet);
-    println!("{}", sum_of_version(packet));
+    println!("{}", evaluate(&packet));
 }
